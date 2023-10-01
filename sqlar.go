088f8854ae69abd82eba743cc1170c70856caf04
Olivier Mengué <dolmen@cpan.org>
2023-10-02 00:58:34 +0200

    sqlar: add package doc

---
+++ sqlar.go
@@ -0,0 +1,6 @@
+// Module sqlar provides libraries to handle [SQLite Archive Files].
+//
+// See [github.com/dolmen-go/sqlar/sqlarfs], an implementation of [io/fs.FS] for reading sqlar files.
+//
+// [SQLite Archive Files]: https://sqlite.org/sqlar.html
+package sqlar
