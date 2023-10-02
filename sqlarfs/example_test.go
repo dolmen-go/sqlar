801730cf1270c4b60f0ef4a2ca5dce104a97c926
Olivier Mengué <dolmen@cpan.org>
2023-10-02 23:14:43 +0200

    sqlarfs: add runnable example

---
+++ sqlarfs/example_test.go
@@ -0,0 +1,45 @@
+package sqlarfs_test
+
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+
+	"github.com/dolmen-go/sqlar/sqlarfs"
+)
+
+func Example() {
+	db, err := sql.Open(sqliteDriver, "testdata/simple.sqlar")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// List files in the archive, recusively
+	ar := sqlarfs.New(db, sqlarfs.PermOwner)
+	fs.WalkDir(ar, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		info, _ := d.Info()
+		fmt.Printf("%s %4d %s  %s\n", info.Mode(), info.Size(), info.ModTime().UTC().Format("_2 Jan 2006 15:04"), info.Name())
+		return nil
+	})
+
+	// Dump one file
+	f, err := ar.Open("foo.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("foo.txt:")
+	io.Copy(os.Stdout, f)
+	// Output:
+	// dr-xr-xr-x    0  1 Jan 1970 00:00  .
+	// -rw-r--r--    4 30 Sep 2023 14:51  bar.txt
+	// -rw-r--r--    4 30 Sep 2023 16:14  foo.txt
+	// foo.txt:
+	// Foo
+}
